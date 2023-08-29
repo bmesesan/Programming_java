91414e340aea1657dd8f1c6e97d3fb4af1ab40e1
bmesesan <mesesan.boby@yahoo.com>
2023-08-29 16:12:31 +0300

    commit BST construction.

--- src/algo/expert/bst/BSTConstruction.java
+++ src/algo/expert/bst/BSTConstruction.java
@@ -1,5 +1,48 @@
 package algo.expert.bst;
+import java.util.*;
 
-public class BSTConstruction {
+class BSTConstruction {
+    static class BST {
+        public int value;
+        public BST left;
+        public BST right;
 
+        public BST(int value) {
+            this.value = value;
+        }
+
+        public BST insert(int value) {
+			// Write your code here.
+			// Do not edit the return statement of this method.
+        	boolean posFound = false;
+        	BST currBSTRef = this;
+  
+        	
+        	while (!posFound) {
+        		if (currBSTRef == null) {
+        			posFound = true;
+        			currBSTRef = new BST(value);
+        		} else {
+        			if (value >= currBSTRef.value) {
+        				currBSTRef = currBSTRef.right;
+        			} else {
+        				currBSTRef = currBSTRef.left;
+        			}
+        		}
+        	}
+        	
+			return currBSTRef;
+		}
+
+        public boolean contains(int value) {
+            // Write your code here.
+            return false;
+        }
+
+        public BST remove(int value) {
+            // Write your code here.
+            // Do not edit the return statement of this method.
+            return this;
+        }
+    }
 }
